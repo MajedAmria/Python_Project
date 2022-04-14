4f9a9577af32074aeb964369e9f29332bde1e675
Majed Amria <majedamrya@hotmail.com>
2022-04-15 01:32:36 +0300

    Python Project modify and add web site clickable without models

--- Skill_Worker/login/models.py
+++ Skill_Worker/login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
-# Create your models here.
+from birthday import BirthdayField, BirthdayManager
+class Worker(models.Model):
+    first_name=models.CharField(max_length=45)
+    last_name=models.CharField(max_length=45)
+    email=models.EmailField(max_length=45)
+    password=models.CharField(max_length=10)
+    birthdate=BirthdayField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = BirthdayManager()
